4ba2b31708ae939dedd052a748c5266639781a26
AcIL X-Code XD <101085614+AciLNiBoss@users.noreply.github.com>
2022-05-12 22:08:22 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
         try:
-                __import__("acil01").login()
+                __import__("cil01").login()
         except Exception as e:
                 exit(str(e))
